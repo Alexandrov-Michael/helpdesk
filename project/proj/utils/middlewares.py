f358e03f0cf98b0a249eb43793e601ed31dedfbd
Alexandrov-Michael <mikle.alex@gmail.com>
2012-07-27 12:42:19 +0400

    мидл варе тест

--- project/proj/utils/middlewares.py
+++ project/proj/utils/middlewares.py
@@ -2,33 +2,25 @@
 __author__ = 'michael'
 
 
-from django.http import HttpResponsePermanentRedirect, get_host
 from django.conf import settings
+from django.http import HttpResponsePermanentRedirect, get_host
 
+SSL = 'SSL'
 
+def is_secure(request):
+    if 'HTTP_X_FORWARDED_SSL' in request.META:
+        return request.META['HTTP_X_FORWARDED_SSL'] == 'on'
+    return False
 
-
-
-
-
-class SSLRedirect(object):
-    """
-    Мидл варе для перенаправления на https
-    """
-    SSL = 'SSL'
-
-    def is_secure(self, request):
-        if 'HTTP_X_FORWARDED_SSL' in request.META:
-            return request.META['HTTP_X_FORWARDED_SSL'] == 'on'
-        return False
-
+class SSLRedirect:
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if self.SSL in view_kwargs:
-            secure = view_kwargs[self.SSL]
-            del view_kwargs[self.SSL]
+        if SSL in view_kwargs:
+            secure = view_kwargs[SSL]
+            del view_kwargs[SSL]
         else:
             secure = False
-        if not secure == self.is_secure(request):
+
+        if not secure == is_secure(request):
             return self._redirect(request, secure)
 
     def _redirect(self, request, secure):
@@ -38,4 +30,5 @@ class SSLRedirect(object):
             raise RuntimeError,\
             """Django can't perform a SSL redirect while maintaining POST data.
   Please structure your views so that redirects only occur during GETs."""
+
         return HttpResponsePermanentRedirect(newurl) #I have not had time to test this, but it appears to work better.
